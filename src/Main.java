4fb6e67b408fbee8f5ac445c5748c48b18149ece
amschwartz <amschwartz87@gmail.com>
2021-10-06 10:38:25 +0300

    programm

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,9 @@
+
 public class Main {
+    public static void main(String[] args) {
+        int priceTicket = 10_000;
+        int amountRub = 20;
+        int mile = priceTicket / amountRub;
+        System.out.println("Количество милей:" + mile);
+    }
 }
